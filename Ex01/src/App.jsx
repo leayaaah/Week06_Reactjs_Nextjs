de052bc985eca1d07f95e9337029340f1afb5d1b
H82M19\Student <Student>
2026-04-17 08:27:01 +0700

    Ex01: fix main: restore

--- Ex01/src/App.jsx
+++ Ex01/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { RecoilRoot } from 'recoil';
-import ComponentA from '../../Ex02/src/components/ComponentA';
-import ComponentB from '../../Ex02/src/components/ComponentB';
+import ComponentA from './components/ComponentA';
+import ComponentB from './components/ComponentB';
 
 function App() {
   return (
